ced498e75f7a8793976ce289a6df7bc2d73970a5
Jake <jacobwebber@filmskills.com.au>
2014-09-19 13:16:02 +1000

    Split everything off into its own class for ease of use. Setup top level instance vars

--- thumbnails.rb
+++ thumbnails.rb
@@ -23,49 +23,66 @@ class Thumbnail
 		# complete initial setup for the script to run. Enter AWS credentials, and the buckets required.
 		AWS.config(YAML.load_file('./config/aws-s3.yaml'))
 		s3 = AWS::S3.new
-		@original_bucket = s3.buckets['learningoriginal'] 
-		@thumbnail_bucket = s3.buckets['learningthumb'] 
+
+		@source_bucket = 'learningoriginal'
+		@desintation_bucket = 'learningcontent'
+
+		@bucket_in = s3.buckets[@source_bucket] 
+		@bucket_out = s3.buckets[@desintation_bucket]
+		@compression_type = 'web'
+
+		@out_entity = :web
+
 	end
 
+
 	def find
-		#unq = Content.where("thumbnail = 'default.png' and type = 'Graphic'")
-		unq = Content.where("id = 203")
-		puts "There are #{unq.count} pieces of content that do not have thumbnails."
+		unq = Content.where("web IS NULL and type = 'Graphic'")
+
+		puts "There are #{unq.count} pieces of content that will be compressed."
 
 		unq.each do |record|
-			if CheckExistence.new.check('learningoriginal', record.original) == true
-				puts "Beginning compression process for content #{record.id}."
-				thumbnail = compress(record.original)
-				#puts "Updating database with newly compressed thumbnail for record #{record.id}."
-				#Content.update(record.id, :thumbnail => thumbnail)
-				puts 'Finished.'
-			elsif CheckExistence.new.check('learningoriginal', record.original) == false
-				puts "The destination file #{record.id} (#{record.original}) was not found on s3. Skipping this file."
-			else
-				puts "There was an error interfacing with s3 to check if the objects exists."
+
+			# lets check to see if the original file exists in s3, if it does, commence compression.
+			puts "Checking if '#{record.original}' exists in '#{@source_bucket}' bucket"
+			 if CheckExistence.new.check(@source_bucket, record.original) == true
+			 	puts "Located '#{record.original}' file."
+			 	puts "Beginning compression process for content #{record.id}."
+			 	compressed_filename = send_for_compression(record.original)
+			 	puts "Updating #{@out_entity} record in database with newly compressed file for record #{record.id}."
+			 	Content.update(record.id, @out_entity => compressed_filename)
+			 	puts 'Finished.'
+			 elsif CheckExistence.new.check(@source_bucket, record.original) == false
+			 	puts "The destination file #{record.id} (#{record.original}) was not found on s3. Skipping this file."
+			 else
+			 	puts "There was an error interfacing with s3 to check if the objects exists."
 			end
 		end
 	end
 
-	def compress(key)
+	def send_for_compression(key)
 		# generate a unique file name for the file we want to compress.
-		original_file = SecureRandom.hex(6) + '.jpg'
+		original_file = SecureRandom.hex(6)
 
 		# download the file off s3 to a local directory, name it the hex we created above.
 		File.open("./compression/#{original_file}.png", 'wb') do |file|
-		  @original_bucket.objects[key].read do |chunk|
+		  @bucket_in.objects[key].read do |chunk|
 		    file.write(chunk)
 		  end
 		end
 
-		# kick off the compression
-		compressed_file = ImageCompress.new.compress(original_file, 'thumbnail')
+		# kick off the compression using our imagecompression class,
+		# we pass the file that we want compressed as well as type of compression
+		# (this is used to determine compression settings.)
+		compressed_file = ImageCompress.new.compress(original_file, @compression_type)
 
-		# send compressed file to S3 bucket
-		puts "Uploading new thumbnail (#{compressed_file}) to s3 bucket."
-
-		obj = @thumbnail_bucket.objects["#{compressed_file}"]
+		# # send compressed file to S3 bucket.
+		puts "Uploading new file (#{compressed_file}) to s3 bucket(#{@desintation_bucket})."
+		obj = @bucket_out.objects["#{compressed_file}"]
 		obj.write(Pathname.new("./compression/#{compressed_file}"))
+
+		puts 'Upload Complete.'
+		return compressed_file
 
 	end
 
