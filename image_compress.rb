1eae7ea6cb0a2fa71a294d3f31d413fd50bb1dc2
Jake <jacobwebber@filmskills.com.au>
2014-09-19 11:22:46 +1000

    reworked, added comments

--- image_compress.rb
+++ image_compress.rb
@@ -5,6 +5,7 @@ class ImageCompress
  	require 'mini_magick'
 
 	def compress(file,type)
+		
 		# calculate the initial size of the file prior to compression.
 		original_size = File.size("./compression/#{file}.png") / 1024.00
 		puts "Commencing compression of file '#{file}' (#{original_size.floor}kb)."
@@ -15,10 +16,10 @@ class ImageCompress
 		compressed_file = SecureRandom.hex(6) + '.jpg'
 		image = MiniMagick::Image.open("./compression/#{file}.png")
 
-		if type == 'thumbnail'
-			specs = get_specs('thumbnail')
-		end
+		# request specs from the 'get_specs' method of this class.
+		specs = get_specs(type)
 
+		# pass the specs to the minimagick gem
 		image.format specs[0]
 		image.quality specs[1]
 		image.resize specs[2]
@@ -33,9 +34,14 @@ class ImageCompress
 		puts "The file was reduced by #{compression_rate.floor}%."
 
 		return compressed_file
+		
 	end
 
 	def get_specs(type)
+
+		# this method supplies different specs of the compression operation based on what is passed to it.
+		# this returns an array to the caller with the following parameters:
+		# [#format_of_output, #quality_of_compression, #resize_percentage]
 
 		if type == 'thumbnail'
 			specs = ['jpg', '50', '40%']
